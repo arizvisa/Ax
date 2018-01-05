c5c044ca8d9861bf5d1e5d1da82974e17a6bbeb2
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-01-05 01:06:38 -0600

    Shuffled a couple of errors from other modules into this one.

--- js/errors.js
+++ js/errors.js
@@ -16,3 +16,25 @@ errors.create({
     defaultExplanation: 'This class is missing the implementation of a required method.',
     parent: errors.NotImplementedError,
 });
+
+errors.create({
+    name: 'NativeError',
+    defaultExplanation: 'An internal error has occurred.',
+});
+
+errors.create({
+    name: 'IntegerError',
+    defaultExplanation: 'An integer error has occurred.',
+    parent: errors.NativeError,
+});
+
+errors.create({
+    name: 'FloatingPointError',
+    defaultExplanation: 'A floating-point error has occurred.',
+    parent: errors.NativeError,
+});
+
+errors.create({
+    name: 'AssertionError',
+    defaultExplanation: 'An assertion has failed.',
+});
