ecaaa0daf56f12c0ee68d7eb2fe5be6c4b4d1211
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-01-12 00:44:08 -0600

    Replaced the camelCase methods in jtypes with lowercase properties.
    errors.js:
        Replaced the NativeError class with RuntimeError.
        Added some new exceptions to throw during certain situations.
    
    jtypes.js:
        Replaced the .classname property with a static method .typename
            which is called by .classname.
        Reworked .new() so that it doesn't use a side-effect for .parent.
        Replaced all the camelCase methods with lowercase properties.
        Replaced all calls to constructors with calls to the .new method.
        Renamed the .getString() implementation to .str().

--- js/errors.js
+++ js/errors.js
@@ -1,8 +1,19 @@
 import * as errors from 'errors';
 
+/** Base classes that all exception types are derived from. */
 errors.create({
     name: 'NotImplementedError',
     defaultExplanation: 'Missing implementation.',
+});
+
+errors.create({
+    name: 'StaticError',
+    defaultExplanation: 'A static error has occurred.',
+});
+
+errors.create({
+    name: 'RuntimeError',
+    defaultExplanation: 'A runtime error has occurred.',
 });
 
 errors.create({
@@ -18,23 +29,38 @@ errors.create({
 });
 
 errors.create({
-    name: 'NativeError',
-    defaultExplanation: 'An internal error has occurred.',
+    name: 'PrivateMemberError',
+    defaultExplanation: 'This member is private and may not be accessed.',
+    parent: errors.StaticError,
+});
+
+errors.create({
+    name: 'ProtectedMemberError',
+    defaultExplanation: 'This member is protected and may not be modified.',
+    parent: errors.StaticError,
 });
 
 errors.create({
     name: 'IntegerError',
     defaultExplanation: 'An integer error has occurred.',
-    parent: errors.NativeError,
+    parent: errors.RuntimeError,
 });
 
 errors.create({
     name: 'FloatingPointError',
     defaultExplanation: 'A floating-point error has occurred.',
-    parent: errors.NativeError,
+    parent: errors.RuntimeError,
 });
 
 errors.create({
     name: 'AssertionError',
     defaultExplanation: 'An assertion has failed.',
+    parent: errors.StaticError,
 });
+
+errors.create({
+    name: 'MemoryError',
+    defaultExplanation: 'An invalid memory access has occurred.',
+    parent: errors.RuntimeError,
+});
+
--- js/jtypes.js
+++ js/jtypes.js
@@ -14,17 +14,17 @@ errors.create({
 errors.create({
     name: 'InvalidSizeError',
     defaultExplanation: 'An invalid size has been specified.',
-    parent: errors.NativeError,
+    parent: errors.IntegerError,
 });
 errors.create({
     name: 'InvalidAddressError',
     defaultExplanation: 'An invalid address has been specified.',
-    parent: errors.NativeError,
+    parent: errors.MemoryError,
 });
 errors.create({
     name: 'UninitializedError',
     defaultExplanation: 'This instance is not currently initialized.',
-    parent: errors.NativeError,
+    parent: errors.RuntimeError,
 });
 
 /* General utility functions */
@@ -63,17 +63,71 @@ function ofCharCode(n) {
     }
 }
 
-/* Base class definitions */
-export class Jatomic {
-    get classname() { return "Jatomic"; }
-    get Size() {
-        throw new errors.PropertyNotImplementedError('Size');
-    }
+/** Base class for everything **/
+class Jtype {
+    static typename() { return 'undefined'; }
+    get classname() { return this.constructor.typename(); }
+
+    /* new type(address, ?parent)
+     * can set .parent here in case instantiation of a sub-class requires
+     * prior knowledge of another type relative to this one.
+     */
     constructor(address, parent=undefined) {
         [this.address, this.parent] = [address, parent];
     }
+    /* .new(type, address, ?parent)
+     * just a wrapper around the constructor. this wrapper modifies the arguments
+     * to include the method owner as the .parent of the newly created type.
+     */
+    new(type, ...args) {
+        const res = (args.length < 2)? args.concat([this]) : args;
+        return new type(...res);
+    }
+    /* getter/setter for the .value property  */
+    get value() {
+        throw new errors.PropertyNotImplementedError('value');
+    }
+    set value(n) {
+        throw new errors.PropertyNotImplementedError('value');
+    }
+    // methods that a sub-class needs to implement
     bytes() {
-        let [integral, cb] = [this.getValue(), this.getSize()];
+        throw new errors.MethodNotImplementedError('bytes');
+    }
+    serialize() {
+        throw new errors.MethodNotImplementedError('serialize');
+    }
+    summary() {
+        throw new errors.MethodNotImplementedError('summary');
+    }
+    repr() {
+        throw new errors.MethodNotImplementedError('repr');
+    }
+}
+
+/* Basic class definitions for the user */
+export class Jatomic extends Jtype {
+    static typename() { return 'Jatomic'; }
+    get Size() {
+        throw new errors.UndefinedFieldError('Size');
+    }
+    /* Atomic types interact with their .value on-demand. */
+    get value() {
+        throw new errors.PropertyNotImplementedError('value');
+    }
+    set value(n) {
+        throw new errors.PropertyNotImplementedError('value');
+    }
+    /* Atomic types are constant-sized as well. */
+    get size() {
+        return this.Size;
+    }
+    set size(n) {
+        throw new this.ProtectedMemberError('size');
+    }
+    /* regular methods */
+    bytes() {
+        let [integral, cb] = [this.value, this.size];
         let res = [];
         while (cb--) {
             res.push(integral % 256);
@@ -82,7 +136,7 @@ export class Jatomic {
         return res;
     }
     serialize() {
-        let [integral, cb] = [this.getValue(), this.getSize()];
+        let [integral, cb] = [this.value, this.size];
         let res = [];
         while (cb--) {
             let ch = integral % 256;
@@ -90,35 +144,29 @@ export class Jatomic {
             integral = integral / 256;
         }
         return Lazy.default(res)
-                   .join("");
-    }
-    getAddress() {
-        return this.address;
-    }
-    getSize() {
-        return this.Size;
-    }
-    getValue() {
-        throw new errors.MethodNotImplementedError('getValue');
+                   .join('');
     }
     dump() {
-        return Lazy.default(Ax.load(this.getAddress(), this.getSize()))
-                   .map(Ax.toHex).join(" ");
+        return Lazy.default(Ax.load(this.address, this.size))
+                   .map(Ax.toHex).join(' ');
     }
     summary() {
-        return this.getValue().toString();
+        return this.value.toString();
     }
     repr() {
-        let ea = Ax.toHex(this.getAddress());
+        let ea = Ax.toHex(this.address);
         let value = this.summary();
         return `[${ea}] <${this.classname}> : ${value}`;
+    }
+    int() {
+        return this.value;
     }
 }
 
 export class Jatomicu extends Jatomic {
-    get classname() { return "Jatomicu"; }
-    getValue() {
-        let [ofs, cb] = [this.getAddress(), this.getSize()];
+    static typename() { return 'Jatomicu'; }
+    get value() {
+        let [ofs, cb] = [this.address, this.size];
         switch (cb) {
             case 1:
                 return Ax.loadui(ofs, 1);
@@ -137,17 +185,16 @@ export class Jatomicu extends Jatomic {
         throw new errors.InvalidSizeError(`Invalid size ${cb} for an unsigned integer was requested.`);
     }
     summary() {
-        let value = this.getValue();
-        let value_x = Ax.toHex(value);
-        let value_s = value.toString();
+        let res = this.value;
+        let [value_x, value_s] = [Ax.toHex(res), res.toString()];
         return `${value_x} (${value_s})`;
     }
 }
 
 export class Jatomics extends Jatomic {
-    get classname() { return "Jatomics"; }
-    getValue() {
-        let [ofs, cb] = [this.getAddress(), this.getSize()];
+    static typename() { return 'Jatomics'; }
+    get value() {
+        let [ofs, cb] = [this.address, this.size];
         switch (cb) {
             case 1:
                 return Ax.loadsi(ofs, 1);
@@ -168,15 +215,14 @@ export class Jatomics extends Jatomic {
         throw new errors.InvalidSizeError(`Invalid size ${cb} for a signed integer was requested.`);
     }
     summary() {
-        let value = this.getValue();
-        let value_x = Ax.toHex(value);
-        let value_s = value.toString();
+        let res = this.value;
+        let [value_x, value_s] = [Ax.toHex(res), res.toString()];
         return `${value_x} (${value_s})`;
     }
 }
 
 export class Jpointer extends Jatomicu {
-    get classname() { return "Jpointer"; }
+    static typename() { return 'Jtype*'; }
     get Size() {
         return 4;
     }
@@ -190,142 +236,133 @@ export class Jpointer extends Jatomicu {
         return ea;
     }
     dereference() {
-        let t = this.Type;
-        let ea = this.calculate(this.getValue());
-        return new t(ea, this);
+        const [t, ea] = [this.Type, this.calculate(this.int())];
+        return this.new(t, ea);
     }
     summary() {
-        let value = this.getValue();
-        let value_x = Ax.toHex(value);
-        let type = this.Type;
-        let type_s = new type().classname;
+        const [res, type] = [this.value, this.Type];
+        let [value_x, type_s] = [Ax.toHex(res), type.typename()];  // FIXME: is there a better way to get the classname than calling a constructor?
         return `${value_x} -> ${type_s}`;
     }
 }
 
-export class Jcontainer {
-    get classname() { return "Jcontainer"; }
-    constructor(address, parent=undefined) {
-        [this.address, this.parent] = [address, parent];
-        this.value = [];
-        this.indices = {};
+export class Jcontainer extends Jtype {
+    static typename() { return 'Jcontainer'; }
+
+    /* Container types cache all the types that are contained. */
+    get value() { return this._value; }
+    set value(n) { throw new this.ProtectedMemberError('value'); }
+    /* Container types are always honest about their length... */
+    get length() { return this._value.length; }
+    set length(n) { throw new this.ProtectedMemberError('length'); }
+    /* ...and also their size. */
+    get size() {
+        return this._value.reduce((total, instance) => total + instance.size, 0);
     }
-    new(t, ...args) {
-        let res = new t(...args);
-        res.parent = this;
-        return res;
+    set size(n) { throw new this.ProtectedMemberError('size'); }
+
+    constructor(...args) {
+        super(...args);
+        this._value = [];
+        this._indices = {};
     }
     bytes() {
-        return Lazy.default(this.value)
+        const res = this._value;
+        return Lazy.default(res)
                    .map(n => n.bytes())
                    .flatten()
                    .toArray();
     }
     serialize() {
-        return Lazy.default(this.value)
+        const res = this._value;
+        return Lazy.default(res)
                    .map(n => n.serialize())
-                   .join("");
-    }
-    getAddress() {
-        return this.address;
-    }
-    getLength() {
-        return this.value.length;
-    }
-    getSize() {
-        return this.value.reduce((total, instance) => total + instance.getSize(), 0);
-    }
-    getValue() {
-        return this.value;
+                   .join('');
     }
     field(name) {
-        let index = this.indices[name];
-        return this.value[index];
+        const index = this._indices[name];
+        return this._value[index];
     }
     dump() {
-        return Lazy.default(Ax.load(this.getAddress(), this.getSize()))
+        const res = Ax.load(this.address, this.size);
+        return Lazy.default(res)
                    .map(Ax.toHex)
-                   .join(" ");
+                   .join(' ');
     }
     summary() {
         return this.dump();
     }
     repr() {
-        let ea = Ax.toHex(this.getAddress());
-        let value = this.summary();
+        const [ea, value] = [Ax.toHex(this.address), this.summary()];
         return `[${ea}] <${this.classname}> : ${value}`;
     }
 }
 
 /* Base container definitions */
 export class Jarray extends Jcontainer {
-    get classname() { return "Jarray"; }
+    static typename() { return 'Jarray'; }
     get Type() {
         throw new errors.UndefinedFieldError('Type');
     }
     get Length() {
         throw new errors.UndefinedFieldError('Length');
     }
-    constructor(address, parent=undefined) {
-        super(address, parent);
-        let object = this.Type;
-        let count = this.Length;
-        let indices = this.indices;
+    constructor(...args) {
+        super(...args);
 
-        let ea = this.getAddress();
+        const object = this.Type;
+        let [count, indices, value] = [this.Length, this._indices, this.value];
+
+        let ea = this.address;
         while (count) {
-            let res = new object(ea, this);
-            indices[this.value.length] = this.value.length;
-            this.value.push(res);
-            ea += res.getSize();
+            let res = this.new(object, ea);
+            indices[value.length] = value.length;
+            value.push(res);
+            ea += res.size;
             count -= 1;
         }
     }
 }
 
 export class Jstruct extends Jcontainer {
-    get classname() { return "Jstruct"; }
+    static typename() { return 'Jstruct'; }
     get Fields() {
         throw new errors.UndefinedFieldError('Fields');
     }
+    constructor(...args) {
+        super(...args);
+        const fields = this.Fields;
 
-    constructor(address, parent=undefined) {
-        super(address, parent);
-        let fields = this.Fields;
-
-        const indices = this.indices;
-
-        let ea = this.getAddress();
+        let [indices, value] = [this._indices, this.value];
+        let ea = this.address;
         fields.map(
             field => {
                 let [name, type] = field;
-                let res = new type(ea, this);
-                indices[name] = this.value.length;
-                this.value.push(res);
-                ea += res.getSize();
+                let res = this.new(type, ea);
+                indices[name] = value.length;
+                value.push(res);
+                ea += res.size;
             }
         );
     }
     repr() {
-        let ea = this.getAddress();
-        let fields = this.Fields;
+        const fields = this.Fields;
+        let [ea, value] = [this.address, this.value];
+
         let result = [];
         result.push(`<${this.classname}>`);
-        for (let i=0; i < this.value.length; i++) {
-            let addr = Ax.toHex(ea);
-            let [name, _] = fields[i];
-            let value = this.value[i];
-            let summary = value.summary();
-            result.push(`[${addr}] "${name}" <${value.classname}> : ${summary}`);
-            ea += this.value[i].getSize();
+        for (let i=0; i < value.length; i++) {
+            let [ea_x, [name, _], val] = [Ax.toHex(ea), fields[i], value[i]];
+            result.push(`[${ea_x}] "${name}" <${val.classname}> : ${val.summary()}`);
+            ea += val.size;
         }
-        return result.join("\n");
+        return result.join('\n');
     }
 }
 
 /* Dynamic container types */
 export class Jtarray extends Jcontainer {
-    get classname() { return "Jtarray"; }
+    static typename() { return 'Jtarray'; }
     get Type() {
         throw new errors.UndefinedFieldError('Type');
     }
@@ -335,103 +372,103 @@ export class Jtarray extends Jcontainer {
     isTerminator(value) {
         throw new errors.MethodNotImplementedError('IsTerminator');
     }
-    constructor(address, parent=undefined) {
-        super(address, parent);
-        let object = this.Type;
-        let indices = this.indices;
-        let ea = this.getAddress();
+    constructor(...args) {
+        super(...args);
+
+        const object = this.Type;
+        let [ea, indices, value] = [this.address, this._indices, this.value];
 
         let res;
         do {
-            res = new object(ea, this);
-            indices[this.value.length] = this.value.length;
-            this.value.push(res);
-            ea += res.getSize();
+            res = this.new(object, ea);
+            indices[value.length] = value.length;
+            value.push(res);
+            ea += res.size;
         } while (!this.isTerminator(res));
     }
 }
 
 export class Jstring extends Jarray {
-    get classname() { return "Jstring"; }
+    static typename() { return 'Jstring'; }
     get Type() { return Juint8; }
     get Length() {
         throw new errors.UndefinedFieldError('Length');
     }
-    getString() {
+    str() {
+        /* XXX: ensure this returns an ascii-only string. */
         return Lazy.default(this.value)
-                   .map(n => n.getValue())
+                   .map(n => n.value)
                    .map(String.fromCharCode)
-                   .join("");
+                   .join('');
     }
     summary() {
         return Lazy.default(this.value)
-                   .map(n => n.getValue())
+                   .map(n => n.value)
                    .map(ofCharCode)
-                   .join("");
+                   .join('');
     }
     repr() {
-        let ea = Ax.toHex(this.getAddress());
-        let value = this.summary();
-        return `[${ea}] <${this.classname}> : "${value}"`;
+        let [ea_x, value_s] = [Ax.toHex(this.address), this.summary()];
+        return `[${ea_x}] <${this.classname}> : "${value_s}"`;
     }
 }
 
 export class Jszstring extends Jtarray {
-    get classname() { return "Jszstring"; }
+    static typename() { return 'Jszstring'; }
     get Type() { return Juint8; }
-    isTerminator(value) {
-        return value.getValue() == 0;
+    isTerminator(object) {
+        return object.value == 0;
     }
-    getString() {
+    str() {
+        /* XXX: ensure this returns an ascii-only string. */
         return Lazy.default(this.value)
-                   .map(n => n.getValue())
+                   .map(n => n.value)
                    .map(String.fromCharCode)
                    .slice(0, -1)
-                   .join("");
+                   .join('');
     }
     summary() {
         return Lazy.default(this.value)
-                   .map(n => n.getValue())
+                   .map(n => n.value)
                    .map(ofCharCode)
-                   .join("");
+                   .join('');
     }
     repr() {
-        let ea = Ax.toHex(this.getAddress());
-        let value = this.summary();
-        return `[${ea}] <${this.classname}> : "${value}"`;
+        let [ea_x, value_s] = [Ax.toHex(this.address), this.summary()];
+        return `[${ea_x}] <${this.classname}> : "${value_s}"`;
     }
 }
 
 /* Atomic types */
 export class Juint8 extends Jatomicu {
-    get classname() { return "Juint8"; }
+    static typename() { return 'Juint8'; }
     get Size() { return 1; }
 }
 export class Juint16 extends Jatomicu {
-    get classname() { return "Juint16"; }
+    static typename() { return 'Juint16'; }
     get Size() { return 2; }
 }
 export class Juint32 extends Jatomicu {
-    get classname() { return "Juint32"; }
+    static typename() { return 'Juint32'; }
     get Size() { return 4; }
 }
 export class Juint64 extends Jatomicu {
-    get classname() { return "Juint64"; }
+    static typename() { return 'Juint64'; }
     get Size() { return 8; }
 }
 export class Jsint8 extends Jatomics {
-    get classname() { return "Jsint8"; }
+    static typename() { return 'Jsint8'; }
     get Size() { return 1; }
 }
 export class Jsint16 extends Jatomics {
-    get classname() { return "Jsint16"; }
+    static typename() { return 'Jsint16'; }
     get Size() { return 2; }
 }
 export class Jsint32 extends Jatomics {
-    get classname() { return "Jsint32"; }
+    static typename() { return 'Jsint32'; }
     get Size() { return 4; }
 }
 export class Jsint64 extends Jatomics {
-    get classname() { return "Jsint64"; }
+    static typename() { return 'Jsint64'; }
     get Size() { return 8; }
 }
